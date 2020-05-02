b283b2849b885596ae9929b3441c4785144fcbce
iskandarherputra <iskandarherputra@gmail.com>
2020-05-02 14:26:20 +0700

    Initial Commit

---
+++ DicodingPortoFolio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DicodingPortoFolio
+//
+//  Created by Iskandar Herputra Wahidiyat on 02/05/20.
+//  Copyright © 2020 Iskandar Herputra Wahidiyat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
